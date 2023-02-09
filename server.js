1af80d813f6689840cfb526177720e8fb9ba4632
Tin Nguyen <nguyentritin211@gmail.com>
2023-02-09 13:10:07 +0200

    First commit

--- server.js
+++ server.js
@@ -73,6 +73,6 @@ app.post('/login-user', (req, res) => {
     })
 })
 
-app.listen(3000, (req, res) => {
-    console.log('listening on port 3000......')
+app.listen(4000, (req, res) => {
+    console.log('listening on port 4000......')
 })
